c05ed37157345e0b53ac3768dd16854fd2dcbad9
Burduja <61868906+Burduja@users.noreply.github.com>
2020-11-15 15:09:56 +0100

    Add files via upload

--- MMM-Covid19ITA.js
+++ MMM-Covid19ITA.js
@@ -1,7 +1,6 @@
 Module.register("MMM-Covid19ITA",{
     defaults: {
-        //default variables
-        header: "Covid19 - ITA",
+        header: "Covid19-ITA",
         urlNation: "https://github.com/pcm-dpc/COVID-19/blob/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json",
         urlRegions: "https://github.com/pcm-dpc/COVID-19/blob/master/dati-json/dpc-covid19-ita-regioni-latest.json",
         animationSpeed: 3000,
@@ -9,9 +8,8 @@ Module.register("MMM-Covid19ITA",{
         updateInterval: 1000 * 10 * 60, // 10 minutes
         firstUpdate: true,
         statNames : ["Tot. Positive", "New Positive", "Tot. Recovered", "Tot. Dead"],
-        statisticsNation:[]
+        //statisticsNation:[]
         //todayData: false,
-
     },
 
     /*
@@ -32,7 +30,7 @@ Module.register("MMM-Covid19ITA",{
         xhr.onreadystatechange=function() {
             if (xhr.readyState===4 && callback) {
                 console.log("all ok")
-                callback(xhr.responseText)
+                callback(xhr.response)
             }
         }
         xhr.send(null);
@@ -42,26 +40,39 @@ Module.register("MMM-Covid19ITA",{
      * function called when the information on the screen needs to be updated. called by the updateDom() function
      */
     getDom: function (){
-        console.log("About to update Dom");
+        //console.log("About to update Dom");
         var wrapper = document.createElement("div");
         //wrapper.innerHTML = this.config.text;
+        wrapper.className = "wrapper";
+        wrapper.style.maxWidth = "300px";
+
         if (!this.loaded) {
             wrapper.innerHTML = this.translate('LOADING');
             return wrapper;
         }
+
+        var header = document.createElement("header");
+        header.classList.add("small", "bright", "light", "header");
+        header.innerHTML = this.config.header;
+        wrapper.appendChild(header);
+
         var table = document.createElement("table");
+
         for (j = 0; j < 3; j++) {
             var row = document.createElement('tr');
-            var nameCell = document.createElement('td');
-            var totalCell = document.createElement('td');
+            var nameCell = document.createElement('th');
+            var totalCell = document.createElement('th');
 
+            /*
             [nameCell, totalCell].forEach((td, i) => {
                 td.className = 'small';
                 if (i !== 0)
                     td.className += " bright align-right"
             })
+
+             */
             nameCell.innerHTML = this.statNames[j];
-            totalCell.innerHTML = statisticsNation[0];
+            totalCell.innerHTML = this.stats[0];
 
             row.appendChild(nameCell);
             row.appendChild(totalCell);
@@ -76,10 +87,11 @@ Module.register("MMM-Covid19ITA",{
      * function called when all modules are loaded and the system is ready to boot up
      */
     start: function () {
-        console.log("Starting...")
+        Log.info("Starting module: " + this.name);
         this.firstUpdate = true;
         this.loaded = false; // will become true once i'm done processing my info
-        this.scheduleUpdate( this.config.initialLoadDelay);  // update now - can improve
+        this.stats = [];
+        this.scheduleUpdate();  // update now - can improve
         /*
         var self = this;
         setInterval(function () {
@@ -112,17 +124,25 @@ Module.register("MMM-Covid19ITA",{
             if (!data)
                 return;
             console.log("Data received");
+            //this.stats = data;
             this.scheduleUpdate(this.updateInterval);
             this.processStats(data);
-            updateDom();
+            this.updateDom(this.config.animationSpeed);
         })
     },
 
 
     processStats: function (data){
+        /**
         this.statisticsNation[0] = data[0].totale_positivi;
         this.statisticsNation[1] = data[0].nuovi_positivi;
         this.statisticsNation[2] = data[0].dimessi_guariti;
         this.statisticsNation[3] = data[0].deceduti;
+         */
+        this.stats[0] = data[0].totale_positivi;
+        this.stats[1] = data[0].nuovi_positivi;
+        this.stats[2] = data[0].dimessi_guariti;
+        this.stats[3] = data[0].deceduti;
+        this.loaded = true;
     }
     });
